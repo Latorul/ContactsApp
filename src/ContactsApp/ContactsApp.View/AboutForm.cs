08476b1f69b7b2a77325c62b442ecae1c44e796e
Artem <gdtechai@gmail.com>
2023-04-01 04:58:23 +0700

    ui(View): полностью реализована адаптивная вёрстка окон: MainForm, ContactForm, AboutForm

---
+++ src/ContactsApp/ContactsApp.View/AboutForm.cs
@@ -0,0 +1,45 @@
+﻿using System.Diagnostics;
+
+namespace ContactsApp.View;
+
+public partial class AboutForm : Form
+{
+    public AboutForm()
+    {
+        InitializeComponent();
+    }
+
+    private void OkButton_Click(object sender, EventArgs e)
+    {
+        Close();
+    }
+
+    private void IconsReferenceLinkLabel_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+    {
+        IconsReferenceLinkLabel.LinkVisited = true;
+
+        Process.Start(
+            new ProcessStartInfo
+            {
+                FileName = "https://icons8.ru/",
+                UseShellExecute = true
+            });
+    }
+
+    private void GithubLlinkLabel_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+    {
+        GithubLlinkLabel.LinkVisited = true;
+
+        Process.Start(
+            new ProcessStartInfo
+            {
+                FileName = "https://github.com/Latorul",
+                UseShellExecute = true
+            });
+    }
+
+    private void AboutForm_Shown(object sender, EventArgs e)
+    {
+        LicenseTextBox.Text = Properties.Resources.LICENSE;
+    }
+}
---
+++ src/ContactsApp/ContactsApp.View/ContactForm.cs
@@ -0,0 +1,52 @@
+﻿namespace ContactsApp.View;
+
+public partial class ContactForm : Form
+{
+    public ContactForm()
+    {
+        InitializeComponent();
+    }
+
+    #region ContactButtons_Click
+
+    private void AddPhotoButton_Click(object sender, EventArgs e)
+    {
+    }
+
+    private void OkButton_Click(object sender, EventArgs e)
+    {
+        Close();
+    }
+
+    private void CancelButton_Click(object sender, EventArgs e)
+    {
+        Close();
+    }
+
+    #endregion
+
+    #region AddPhotoButton_MouseEvents
+
+    private void AddPhotoButton_MouseEnter(object sender, EventArgs e)
+    {
+        AddPhotoButton.Image = Properties.Resources.add_contact_32x32;
+        AddPhotoButton.BackColor = ColorTranslator.FromHtml("#FAF5F5");
+    }
+
+    private void AddPhotoButton_MouseLeave(object sender, EventArgs e)
+    {
+        AddPhotoButton.Image = Properties.Resources.add_contact_32x32_gray;
+        AddPhotoButton.BackColor = Color.White;
+    }
+
+    #endregion
+
+    private void ContactForm_KeyDown(object sender, KeyEventArgs e)
+    {
+        if (e.KeyCode == Keys.F1)
+        {
+            var form = new AboutForm();
+            form.ShowDialog();
+        }
+    }
+}
