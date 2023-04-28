2ed1b30586547d7a77fd57c4fccf40bb2779bf5d
Artem <gdtechai@gmail.com>
2023-04-28 22:14:58 +0700

    refactor(CountrySelector): координаты для отрисовки элементов вынесены в отдельные переменные

--- src/ContactsApp.View/Controls/CountrySelector.cs
+++ src/ContactsApp.View/Controls/CountrySelector.cs
@@ -1,19 +1,47 @@
 ﻿namespace ContactsApp.View.Controls;
 
+/// <summary>
+/// Выпадающий список, элементы которого содержат флаг и название страны.
+/// </summary>
 public sealed class CountrySelector : ComboBox
 {
+    /// <summary>
+    /// Отступ от краёв до флага.
+    /// </summary>
+    private const int ImageMargin = 1;
+
+    /// <summary>
+    /// Отступ между флагом и названием страны.
+    /// </summary>
+    private const int TextMargin = 2;
+
+
+    /// <summary>
+    /// Отрисовывает элемент выпадающего списка.
+    /// </summary>
     protected override void OnDrawItem(DrawItemEventArgs e)
     {
         e.DrawBackground();
 
-        if (e.Index != -1 && 
+        if (e.Index != -1 &&
             e.Index < Items.Count)
         {
-            CountryDropDownItem item = (CountryDropDownItem)Items[e.Index];
+            var item = (CountryDropDownItem)Items[e.Index];
 
-            e.Graphics.DrawImage(item.Image, e.Bounds.Left + 1, e.Bounds.Top + 1);
-            e.Graphics.DrawString(item.CountryInfo.Name, e.Font, new SolidBrush(e.ForeColor), 
-                e.Bounds.Left + item.Image.Width + 2, e.Bounds.Top);
+            var flagLocation = new Point()
+            {
+                X = e.Bounds.Left + ImageMargin,
+                Y = e.Bounds.Top + ImageMargin
+            };
+            var stringLocation = new Point()
+            {
+                X = e.Bounds.Left + item.Image.Width + TextMargin,
+                Y = e.Bounds.Top
+            };
+
+            e.Graphics.DrawImage(item.Image, flagLocation);
+            e.Graphics.DrawString(item.CountryInfo.Name, e.Font,
+                new SolidBrush(e.ForeColor), stringLocation);
         }
 
         base.OnDrawItem(e);
