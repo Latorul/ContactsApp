113b5f92987bc56182815641a106aa584c1a0f03
Artem <gdtechai@gmail.com>
2023-05-26 20:10:19 +0700

    test(ContactFactory): реализованы все тесты для класса ContactFactory

--- src/ContactsApp.Model/ContactFactory.cs
+++ src/ContactsApp.Model/ContactFactory.cs
@@ -5,55 +5,155 @@
 /// </summary>
 public static class ContactFactory
 {
+    private static readonly List<string> FullNames = new()
+    {
+        "Deborah Reeves",
+        "John Brown",
+        "Brian Jackson",
+        "John Green",
+        "Andrew Green",
+        "Ryan Black",
+        "Mary Jones",
+        "Cindy Lewis",
+        "Joan Chavez",
+        "Martha King",
+        "Bessie Ford",
+        "Norman Jordan",
+        "Linda Anderson",
+        "Brian Flores",
+        "Jessica Williams",
+        "Tyrone Williams"
+    };
+
+    private static readonly List<string> Emails = new()
+    {
+        "deborahreeves@mail.com",
+        "johnbrown@mail.com",
+        "brianjackson@mail.com",
+        "johngreen@mail.com",
+        "andrewgreen@mail.com",
+        "ryanblack@mail.com",
+        "maryjones@mail.com",
+        "cindylewis@mail.com",
+        "joanchavez@mail.com",
+        "marthaking@mail.com",
+        "bessieford@mail.com",
+        "normanjordan@mail.com",
+        "lindaanderson@mail.com",
+        "brianflores@mail.com",
+        "jessicawilliams@mail.com",
+        "tyronewilliams@mail.com"
+    };
+
+    private static readonly List<string> VkId = new()
+    {
+        "https://vk.com/deborahreeves",
+        "https://vk.com/johnbrown",
+        "https://vk.com/brianjackson",
+        "https://vk.com/johngreen",
+        "https://vk.com/andrewgreen",
+        "https://vk.com/ryanblack",
+        "https://vk.com/maryjones",
+        "https://vk.com/cindylewis",
+        "https://vk.com/joanchavez",
+        "https://vk.com/marthaking",
+        "https://vk.com/bessieford",
+        "https://vk.com/normanjordan",
+        "https://vk.com/lindaanderson",
+        "https://vk.com/brianflores",
+        "https://vk.com/jessicawilliams",
+        "https://vk.com/tyronewilliams"
+    };
+
+    private static readonly List<string> PhoneCodes = new();
+
+    static ContactFactory()
+    {
+        var document = JsonDocument.Parse(Properties.Resources.countries);
+        var root = document.RootElement;
+
+        foreach (JsonElement element in root.EnumerateArray())
+        {
+            element.TryGetProperty("PhoneCode", out JsonElement phoneCodeElement);
+            PhoneCodes.Add(phoneCodeElement.GetString());
+        }
+    }
+
     /// <summary>
     /// Добавляет контакты для проверки работоспособности программы.
     /// </summary>
-    public static void GenerateContacts(Project project)
+    /// <param name="project"></param>
+    /// <param name="count"></param>
+    public static void GenerateContacts(Project project, int count = 5)
     {
-        project.Contacts.Add(
-            new Contact
+        for (int i = 0; i < count; i++)
+        {
+            project.Contacts.Add(CreateContact());
+        }
+    }
+
+    public static Contact CreateContact()
+    {
+        return new Contact
+        {
+            FullName = GenerateFullName(),
+            Email = GenerateEmail(),
+            PhoneNumber = GeneratePhoneNumber(),
+            DateOfBirth = GenerateDateOfBirth(),
+            VkId = GenerateVkId()
+        };
+    }
+
+    private static string GenerateFullName()
+    {
+        return FullNames[new Random().Next(FullNames.Count)];
+    }
+
+    private static string GenerateEmail()
+    {
+        return Emails[new Random().Next(Emails.Count)];
+    }
+
+    private static string GeneratePhoneNumber()
+    {
+        var phoneNumber = string.Empty;
+
+        phoneNumber += PhoneCodes[new Random().Next(PhoneCodes.Count)];
+        phoneNumber += " (" + new Random().Next(100, 1000) + ") " + new Random().Next(100, 1000);
+
+        var dualCount = new Random().Next(1, 4);
+        for (int i = 0; i < dualCount; i++)
+        {
+            var hyphenOrSpace = new Random().Next(2);
+            if (hyphenOrSpace % 2 == 0)
             {
-                FullName = "Филатов Мирон",
-                Email = "filatov@mail.ru",
-                PhoneNumber = "+1 (234) 567-89",
-                DateOfBirth = DateTime.Today,
-                VkId = "https://vk.com/filatov"
-            });
-        project.Contacts.Add(
-            new Contact
+                phoneNumber += "-";
+            }
+            else
             {
-                FullName = "Ткачев Артём",
-                Email = "tkachev@mail.ru",
-                PhoneNumber = "+1 (234) 567-89",
-                DateOfBirth = DateTime.Today,
-                VkId = "https://vk.com/tkachev"
-            });
-        project.Contacts.Add(
-            new Contact
-            {
-                FullName = "Козин Марк",
-                Email = "kozin@mail.ru",
-                PhoneNumber = "+1 (234) 567-89",
-                DateOfBirth = DateTime.Today,
-                VkId = "https://vk.com/kozin"
-            });
-        project.Contacts.Add(
-            new Contact
-            {
-                FullName = "Журавлев Владимир",
-                Email = "zhuravlev@mail.ru",
-                PhoneNumber = "+1 (234) 567-89",
-                DateOfBirth = DateTime.Today,
-                VkId = "https://vk.com/zhuravlev"
-            });
-        project.Contacts.Add(
-            new Contact
-            {
-                FullName = "Белоусов Андрей",
-                Email = "belousov@mail.ru",
-                PhoneNumber = "+1 (234) 567-89",
-                DateOfBirth = DateTime.Today,
-                VkId = "https://vk.com/belousov"
-            });
+                phoneNumber += " ";
+            }
+
+            phoneNumber += new Random().Next(10, 100);
+        }
+
+        var lastNumber = new Random().Next(-1, 10);
+        return lastNumber == -1 ? phoneNumber : phoneNumber + " " + lastNumber;
+    }
+
+    /// <summary>
+    /// https://stackoverflow.com/a/194870/18739226
+    /// </summary>
+    /// <returns></returns>
+    private static DateTime GenerateDateOfBirth()
+    {
+        var start = new DateTime(1900, 1, 1);
+        int range = (DateTime.Today - start).Days;
+        return start.AddDays(new Random().Next(range));
+    }
+
+    private static string GenerateVkId()
+    {
+        return VkId[new Random().Next(VkId.Count)];
     }
 }
---
+++ src/Testing/ContactsApp.Model.UnitTests/ContactFactoryTest.cs
@@ -0,0 +1,71 @@
+﻿namespace ContactsApp.Model.UnitTests;
+
+[TestFixture]
+public class ContactFactoryTest
+{
+    [Test(Description = "Проверка создания нового контакта со всеми заполненными полями.")]
+    public void CreateContact_ReturnNewContact()
+    {
+        // Setup
+        Contact actual =
+            // Act
+            ContactFactory.CreateContact();
+
+        // Assert
+        Assert.That(actual, Is.Not.Null);
+        Assert.Multiple(() =>
+        {
+            Assert.That(actual.FullName, Is.Not.Empty);
+            Assert.That(actual.Email, Is.Not.Empty);
+            Assert.That(actual.PhoneNumber, Is.Not.Empty);
+            Assert.That(actual.DateOfBirth, Is.Not.EqualTo(default(DateTime)));
+            Assert.That(actual.VkId, Is.Not.Empty);
+        });
+    }
+
+    [TestCase(-1, 
+        TestName = "Проверка создания списка контактов с отрицательным количеством элементов.")]
+    [TestCase(0, 
+        TestName = "Проверка создания списка контактов с нулевым количеством элементов.")]
+    public void GenerateContacts_SetLessThanOneCount_ContactsNotAdded(int count)
+    {
+        // Setup
+        var project = new Project();
+
+        // Act
+        ContactFactory.GenerateContacts(project, count);
+
+        // Assert
+        Assert.That(project.Contacts, Is.Not.Null);
+        Assert.That(project.Contacts, Has.Count.Zero);
+    }
+
+    [TestCase(1, TestName = "Проверка создания списка контактов с одним элементом.")]
+    [TestCase(2, TestName = "Проверка создания списка контактов с несколькими элементами.")]
+    public void GenerateContacts_SetPositiveCount_ContactsAdded(int count)
+    {
+        // Setup
+        var project = new Project();
+
+        // Act
+        ContactFactory.GenerateContacts(project, count);
+
+        // Assert
+        Assert.That(project.Contacts, Is.Not.Null);
+        Assert.That(project.Contacts, Has.Count.EqualTo(count));
+    }
+
+    [Test(Description = "Проверка создания списка контактов с количеством элементов по умолчанию.")]
+    public void GenerateContacts_SetDefaultCount_ContactsAdded()
+    {
+        // Setup
+        var project = new Project();
+
+        // Act
+        ContactFactory.GenerateContacts(project);
+
+        // Assert
+        Assert.That(project.Contacts, Is.Not.Null);
+        Assert.That(project.Contacts, Has.Count.Not.Zero);
+    }
+}
